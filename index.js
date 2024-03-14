65d9be88519935d6596476bfffcf4d09aa344e3e
Amanpatel6232 <amanpatel.ap79099@gmail.com>
2024-03-14 03:51:55 -0700

    comm

--- index.js
+++ index.js
@@ -7,29 +7,29 @@ const studentmodel=require("./studentSchema")
 require("./mongoose")
 
 
-// app.post("/",async (req,resp)=>{
+app.post("/",async (req,resp)=>{
 
-//     const data=await new studentmodel({rollno:101,name:"Ram",marks:45})
-//     const result=await data.save()
-//     resp.send(result)
+    const data=await new studentmodel({rollno:101,name:"Ram",marks:45})
+    const result=await data.save()
+    resp.send(result)
 
-// })
+})
 app.get("/",async(req,resp)=>{
     const result=await studentmodel.find()
     resp.send(result)
 
 })
-// app.post("/",async(req,resp)=>{
-//     const data=await new studentmodel(req.body)
-//     const result=await data.save()
+app.post("/",async(req,resp)=>{
+    const data=await new studentmodel(req.body)
+    const result=await data.save()
     
-// })
-// app.put("/",async(req,resp)=>{
-//     const data=await studentmodel.updateOne({rollno:req.body.rollno},{$set:req.body})
-//     resp.send(data)
-// })
-// app.delete("/",async(req,resp)=>{
-//     const data=await studentmodel.deleteOne({rollno:req.body.rollno})
-//     resp.send(data)
-// })
+})
+app.put("/",async(req,resp)=>{
+    const data=await studentmodel.updateOne({rollno:req.body.rollno},{$set:req.body})
+    resp.send(data)
+})
+app.delete("/",async(req,resp)=>{
+    const data=await studentmodel.deleteOne({rollno:req.body.rollno})
+    resp.send(data)
+})
 app.listen(5000)
